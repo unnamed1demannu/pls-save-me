50b10a4a92844e634c4e49972b505ca10e88e917
Karena Chow <chowkarena1@gmail.com>
2018-03-06 15:51:30 -0800

    edit player name in makefile

--- player.cpp
+++ player.cpp
@@ -7,7 +7,7 @@
  */
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
-    testingMinimax = false;
+    testingMinimax = true;
 
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
